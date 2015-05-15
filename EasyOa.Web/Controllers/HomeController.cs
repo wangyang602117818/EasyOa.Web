02685de3069ccf1654c9d33e3f77c2eb5ac359dc
wangyang <wang.yang@etaostars.com>
2015-05-15 18:36:00 +0800

    wang

--- EasyOa.Web/Controllers/HomeController.cs
+++ EasyOa.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,32 +17,35 @@ namespace EasyOa.Web.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            string[] str = { " " };
+            string[] str = { " ", "  ", "   ", "    ", "     " };
             string[] lines = System.IO.File.ReadAllLines(Server.MapPath("01.txt"));
             string aname = "", acode = "", sql = "";
             string connStr = "Data Source=127.0.0.1;Initial Catalog=easyoa;User ID=sa;Password=123";
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                foreach (string line in lines)
-                {
-                    string[] code = line.Split(str, StringSplitOptions.RemoveEmptyEntries);
-                    acode = code[0];
-                    if (code.Length == 2)
-                    {
-                        aname = code[1];
-                    }
-                    else
-                    {
-                        aname = code[2];
-                    }
-                    sql = "insert into sys_area(a_name, a_code, qping, jping)values('" + aname + "'," + acode + ",'" + aname.ToSpell() + "','" + aname.ToSpell(true) + "')";
-                    using (SqlCommand cmd = new SqlCommand(sql, conn))
-                    {
-
-                    }
-                }
-
-            }
+            //using (SqlConnection conn = new SqlConnection(connStr))
+            //{
+            //    foreach (string line in lines)
+            //    {
+            //        string[] code = line.Split(str, StringSplitOptions.RemoveEmptyEntries);
+            //        acode = code[0];
+            //        if (code.Length == 2)
+            //        {
+            //            aname = code[1];
+            //        }
+            //        else
+            //        {
+            //            aname = code[2];
+            //        }
+            //        sql = "insert into sys_area(a_name, a_code, qping, jping)values('" + aname + "'," + acode + ",'" + aname.ToSpell() + "','" + aname.ToSpell(true) + "')";
+            //        using (SqlCommand cmd = new SqlCommand(sql, conn))
+            //        {
+            //            conn.Open();
+            //            cmd.ExecuteNonQuery();
+            //            System.IO.File.AppendAllText(Server.MapPath("sqls.txt"), sql);
+            //            conn.Close();
+            //        }
+            //    }
+            //}
+            Response.Write("区".ToSpell());
             return View();
         }
 
