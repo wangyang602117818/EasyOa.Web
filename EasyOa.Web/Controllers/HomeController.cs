96797686a33586cae34cc1d5a4e8dcb7c76f6227
wangyang <wang.yang@etaostars.com>
2015-05-19 16:49:48 +0800

    wnagyang

--- EasyOa.Web/Controllers/HomeController.cs
+++ EasyOa.Web/Controllers/HomeController.cs
@@ -17,40 +17,33 @@ namespace EasyOa.Web.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            //string[] str = { " ", "  ", "   ", "    ", "     " };
-            //string[] lines = System.IO.File.ReadAllLines(Server.MapPath("01.txt"));
-            //string aname = "", acode = "", sql = "";
-            //string connStr = "Data Source=127.0.0.1;Initial Catalog=easyoa;User ID=sa;Password=123";
-            //using (SqlConnection conn = new SqlConnection(connStr))
-            //{
-            //    foreach (string line in lines)
-            //    {
-            //        string[] code = line.Split(str, StringSplitOptions.RemoveEmptyEntries);
-            //        acode = code[0];
-            //        if (code.Length == 2)
-            //        {
-            //            aname = code[1];
-            //        }
-            //        else
-            //        {
-            //            aname = code[2];
-            //        }
-            //        sql = "insert into sys_area(a_name, a_code, qping, jping)values('" + aname + "'," + acode + ",'" + aname.ToSpell() + "','" + aname.ToSpell(true) + "')";
-            //        using (SqlCommand cmd = new SqlCommand(sql, conn))
-            //        {
-            //            conn.Open();
-            //            cmd.ExecuteNonQuery();
-            //            System.IO.File.AppendAllText(Server.MapPath("sqls.txt"), sql);
-            //            conn.Close();
-            //        }
-            //    }
-            //}
-
-            for (int i = 0; i <= 100; i++)
+            string[] str = { " ", "  ", "   ", "    ", "     " };
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("01.txt"));
+            string aname = "", acode = "", sql = "";
+            string connStr = "Data Source=127.0.0.1;Initial Catalog=easyoa;User ID=sa;Password=123";
+            using (SqlConnection conn = new SqlConnection(connStr))
             {
-                string s = "中国 人民'银行,我行,辖 的区" + i;
-
-                Response.Write("全拼" + s.ToSpell() + "<br>简拼:" + s.ToSpell(true)+"<br>");
+                foreach (string line in lines)
+                {
+                    string[] code = line.Split(str, StringSplitOptions.RemoveEmptyEntries);
+                    acode = code[0];
+                    if (code.Length == 2)
+                    {
+                        aname = code[1];
+                    }
+                    else
+                    {
+                        aname = code[2];
+                    }
+                    sql = "insert into sys_area(a_name, a_code, qping, jping)values('" + aname + "'," + acode + ",'" + aname.ToSpell() + "','" + aname.ToSpell(true) + "')";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        System.IO.File.AppendAllText(Server.MapPath("sqls.txt"), sql+"\r\n");
+                        conn.Close();
+                    }
+                }
             }
             return View();
         }
