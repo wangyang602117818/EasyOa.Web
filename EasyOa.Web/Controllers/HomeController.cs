144aa9fc90cc238529dae4875286c5a99c1bf5ba
wangyang <wang.yang@etaostars.com>
2015-05-20 09:50:42 +0800

    wangyang

--- EasyOa.Web/Controllers/HomeController.cs
+++ EasyOa.Web/Controllers/HomeController.cs
@@ -17,34 +17,8 @@ namespace EasyOa.Web.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            string[] str = { " ", "  ", "   ", "    ", "     " };
-            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("01.txt"));
-            string aname = "", acode = "", sql = "";
-            string connStr = "Data Source=127.0.0.1;Initial Catalog=easyoa;User ID=sa;Password=123";
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                foreach (string line in lines)
-                {
-                    string[] code = line.Split(str, StringSplitOptions.RemoveEmptyEntries);
-                    acode = code[0];
-                    if (code.Length == 2)
-                    {
-                        aname = code[1];
-                    }
-                    else
-                    {
-                        aname = code[2];
-                    }
-                    sql = "insert into sys_area(a_name, a_code, qping, jping)values('" + aname + "'," + acode + ",'" + aname.ToSpell() + "','" + aname.ToSpell(true) + "')";
-                    using (SqlCommand cmd = new SqlCommand(sql, conn))
-                    {
-                        conn.Open();
-                        cmd.ExecuteNonQuery();
-                        System.IO.File.AppendAllText(Server.MapPath("sqls.txt"), sql+"\r\n");
-                        conn.Close();
-                    }
-                }
-            }
+           
+            
             return View();
         }
 
