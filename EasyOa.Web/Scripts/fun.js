9bee9f96ab2fb801115ef664e8a0e116fa359569
wangyang <wang.yang@etaostars.com>
2015-05-23 16:45:53 +0800

    wangyang

---
+++ EasyOa.Web/Scripts/fun.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="jquery-1.7.1.js" />
+//增加全局函数
+jQuery.extend({
+    getRandom: function (lowerValue, upperValue) {
+        var choices = upperValue - lowerValue + 1;
+        return Math.floor(Math.random() * choices + lowerValue);
+    }
+});
