4d7b913a654da3893a8f7f3a9f1089922c4c5e06
wangyang <wang.yang@etaostars.com>
2015-06-18 16:30:57 +0800

    dsf

--- EasyOa.Web/Controllers/Hr/HrMaintainController.cs
+++ EasyOa.Web/Controllers/Hr/HrMaintainController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace EasyOa.Web.Controllers.Hr
 {
     public class HrMaintainController : Controller
@@ -13,7 +12,7 @@ namespace EasyOa.Web.Controllers.Hr
 
         public ActionResult Index()
         {
-            Response.Write("okdsfdsfs");
+            
             return View();
         }
 
