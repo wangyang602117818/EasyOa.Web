3523cafbcd927e102823a13b56a9123d81de16ba
wangyang <wang.yang@etaostars.com>
2015-08-06 09:27:11 +0800

    df

--- EasyOa.Web/Controllers/TestController.cs
+++ EasyOa.Web/Controllers/TestController.cs
@@ -14,10 +14,28 @@ namespace EasyOa.Web.Controllers
 
         public ActionResult Index()
         {
-            LogRabbitMq.Enqueue("aaaabbbbcccc");
+            List<Person> list = new List<Person>()
+                {
+                    new Person(){Age=12,Name = "张三"},
+                    new Person(){Age = 13,Name = "李四"}
+                };
+            for (int i = 1; i <= 2; i++)
+            {
+                LogEnqueue.Enqueue(BinarySerializerHelper.ObjectToByteArray("aaaanbbccc"));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                LogEnqueue.Enqueue(list);
+            }
             return View();
         }
 
+    }
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
     }
     
 }
