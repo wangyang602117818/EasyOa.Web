324f4eac6868565adbf9961b4778a56fe6b485f1
wangyang <wang.yang@etaostars.com>
2015-11-19 15:04:46 +0800

    sdf

--- EasyOa.Web/Global.asax.cs
+++ EasyOa.Web/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using EasyOa.Common;
+
 namespace EasyOa.Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -18,7 +21,9 @@ namespace EasyOa.Web
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-           
+
+            //log4net配置文件
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(AppConfig.BasePath + "bin\\log4net.config"));
         }
     }
 }
