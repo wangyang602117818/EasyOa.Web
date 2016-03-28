f78f8a36a882bfaf5ed4e9c8dc2d1a1f044df1ab
wangyang <18518317472@163.com>
2016-03-28 09:28:34 +0800

    f

--- EasyOa.Web/Controllers/HomeController.cs
+++ EasyOa.Web/Controllers/HomeController.cs
@@ -17,8 +17,9 @@ namespace EasyOa.Web.Controllers
     {
         public ActionResult Index(string name ,int? age)
         {
-            
-            
+
+            int a = 1, b = 0;
+            int c = a / b;
             return View();
         }
     }
